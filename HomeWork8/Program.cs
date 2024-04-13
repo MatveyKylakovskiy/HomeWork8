3461c35084d3a7013c4a310106c7b4e9a047bc6c
Matveykylak <matveykylak@gmail.com>
2024-04-13 20:36:30 +0300

    First Task

--- HomeWork8/Program.cs
+++ HomeWork8/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using HomeWork8.FirstTask;
+
+
+ICalculable[] figures = { new Triangle(1, 2, 3), new Triangle(1, 1, 1), new Rectangle(4, 4), new Circle(9), new Circle(3) };
+double summ = 0;
+foreach (var fig in figures)
+{
+    fig.CalculatingArea();
+    summ += fig.CalculatingPerimeter();
+    Console.WriteLine();
+}
+Console.WriteLine($"Summs of perimeters: {summ}");
